887d2c1b64a679b2eacd89410154ef10180b5b3f
Bonitheuri <133132495+theurikarue@users.noreply.github.com>
2023-05-16 19:21:11 +0300

    Update 12_sum_of_even_no.py
    changed code to work efficiently

--- 12_sum_of_even_no.py
+++ 12_sum_of_even_no.py
@@ -1,7 +1,9 @@
-#Python program that takes a list of integers as input and outputs the sum of the even integers in the list.
-sum=0
-for i in range(100):
-    if i%2==0:
-        sum=sum+1
-
-print("sum of even integers is: ",sum)
+#Python program that takes a list of integers as input and outputs the sum of the even integers 
+num_list = []
+even_sum = 0
+for i in range(5):
+    num = int(input("Enter an integer: "))
+    num_list.append(num)
+    if num % 2 == 0:
+        even_sum += num
+print("Sum of even integers:", even_sum)
