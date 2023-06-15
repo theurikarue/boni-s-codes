e2efe1be31914dd5a9d4ded81205f06aea14fd53
Bonitheuri <133132495+theurikarue@users.noreply.github.com>
2023-06-15 03:17:56 -0700

    Update 11.vowelcount.py
    changed code

--- 11.vowelcount.py
+++ 11.vowelcount.py
@@ -1,13 +1,13 @@
+  
 #Python program that takes a string as input and outputs the number of vowels in the string
 def vowel_count(str):
-    count=0
-    vowel=set("aeiouAEIOU")
+    count = 0
+    vowel = set("aeiouAEIOU")
     for alphabet in str:
-        if alphabet in vowel :
-            count=count+1
-print("n of vowels in string :",count)
+        if alphabet in vowel:
+            count = count + 1
+    print("Number of vowels in the string:", count)
 
-str="TheuriKarue"
+str = input("Enter string")
 vowel_count(str)
 
-    
