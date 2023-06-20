59b26da18f1539809daf50ab59b65a67c381f79e
Bonitheuri <133132495+theurikarue@users.noreply.github.com>
2023-06-20 02:52:32 -0700

    Update 16.reverse_list_of_strings.py
    changed code

--- 16.reverse_list_of_strings.py
+++ 16.reverse_list_of_strings.py
@@ -9,3 +9,14 @@ print ("The original list is : " + str(test_list))
 rev = [i[::-1] for i in test_list]
 # printing result
 print ("The reversed string list is : " + str(rev))
+
+
+#alternatively
+def reverse_strings(string_list):
+    reversed_list = [string[::-1] for string in string_list]
+    return reversed_list
+
+# Test the function
+string_list = ["Hello", "World", "Python"]
+reversed_list = reverse_strings(string_list)
+print(reversed_list)
