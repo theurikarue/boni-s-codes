a4d3b3436f4d11246cd9164720841bf36e4917ae
Bonitheuri <133132495+theurikarue@users.noreply.github.com>
2023-06-20 02:50:27 -0700

    Update 15.no_of_words_in_string.py
    changed code

--- 15.no_of_words_in_string.py
+++ 15.no_of_words_in_string.py
@@ -1,1 +1,5 @@
-print(len(input("Enter Input:").split()))
+string=input("Enter desired string: ")
+string=string.split()
+print(len(string))
+#alternatively 
+#print(len(input("Enter Input:").split()))
